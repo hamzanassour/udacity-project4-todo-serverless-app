76564412e1e8d17b1129962a9e1fc5c8138105e3
Hnas <hamza.nassour13@gmail.com>
2022-10-14 00:50:02 +0100

    serverless project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'xiw7l5npnl'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-f8nnmv9z.us.auth0.com',
+  clientId: 'ZNJ80XAaj3ZObPGHM98reBtGp2BZmIWK',
+  callbackUrl: 'http://localhost:3000/callback'
+}
